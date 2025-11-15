dd20c962ea61acecec0037edb5d467f76e91d6ed
TANUKITI <yuha31yuha@gmail.com>
2025-11-16 00:45:30 +0900

    Add components

--- GameRecommender/frontend/src/index.tsx
+++ GameRecommender/frontend/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Nomatch from './components/Nomatch';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +15,8 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Sign-In" element={<SignIn />} />
+        <Route path="/Sign-Up" element={<SignUp />} />
         <Route path="/Nomatch" element={<Nomatch />} />
       </Routes>
     </Router>
