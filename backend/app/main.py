d3090ed06662273e10cceb8b0ffcdbdd4ca98b7f
TANUKITI <yuha31yuha@gmail.com>
2025-12-03 06:53:34 +0900

    Revert "change layout" This reverts commit 28a970a96d050eececa5f60a7e4698d7cdea7b4e.

--- backend/app/main.py
+++ backend/app/main.py
@@ -133,9 +133,7 @@ def get_games():
             "title": "イナズマイレブン 英雄たちのヴィクトリーロード",
             "description": "収集・育成サッカーRPG「イナズマイレブン」シリーズ最新作。 新主人公が活躍する新たな物語、5,400人を超える歴代選手の収集・育成、オンラインの全国大会などが楽しめる。",
             "image": "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2799860/header_japanese.jpg",
-            "price": 8910
-            
-            
+            "price": 8910 
         },
         # ...
     ]
